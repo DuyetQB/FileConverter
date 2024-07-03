57053550604ff50075736d10f8a3e56a01e1dfeb
duyetqb <duyetqb@gmail.com>
2024-07-03 15:51:01 +0700

    update header border-bottom color

--- src/app/components/Header/index.tsx
+++ src/app/components/Header/index.tsx
@@ -64,7 +64,7 @@ export default function Header() {
                     <div className={`${isShowNav ? '':'hidden'} justify-between items-center w-full lg:flex lg:w-auto lg:order-1`} id="mobile-menu-2">
                         <ul className="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
                             {listNavItem.map((rc: listNavItemProps) => (
-                                <Link href={rc.link} key={rc.id} className={`${active == rc.link ? 'border-primary' :'border-transparent'} block py-2 pr-4 pl-3 border-b-2 hover:border-primary `}
+                                <Link href={rc.link} key={rc.id} className={`${active == rc.link ? 'border-primary-500' :'border-transparent'} block py-2 pr-4 pl-3 border-b-2 hover:border-primary-500 transition-all `}
                                 onClick={() => handleItemClick(rc.link)}
                                 >
                                     {rc.title}
