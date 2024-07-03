d60608225d2035cadfcb0173851f6361f59cc54c
duyetqb <duyetqb@gmail.com>
2024-07-03 19:41:08 +0700

    move script into head tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -33,10 +33,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
-        {children}
+      <head>
       <Script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1737962627563882"
      crossOrigin="anonymous"></Script>
+      </head>
+      <body className={inter.className}>
+        {children}
       </body>
     </html>
   );
