d1ee21ae8648ecb046f8d100027e303dc551b6e6
duyetqb <duyetqb@gmail.com>
2024-07-03 17:56:00 +0700

    optimize image seo

--- src/app/components/Banner/index.tsx
+++ src/app/components/Banner/index.tsx
@@ -6,17 +6,25 @@ export default function Banner() {
         <div className='background-caro relative'>
             <div className='pb-2'>
                 <h1 className="text-[2em]">A platform to convert many files type. Save your time</h1>
-                <p className='bg-primary-400 text-white inline'>Convert all files you need. It&rsquo;s online and free</p>
+                <p className='bg-primary-500 text-white inline'>Convert all files you need. It&rsquo;s online and free</p>
             </div>
             <div className='flex justify-center relative'>
-                <img src="/assets/banner-first.png" className='object-cover h-[500px]' />
+                <img src="/assets/banner-first.avif" className='object-cover h-[500px]' alt="image banner"
+                    srcSet="/assets/banner-first.avif 640w,
+                 /assets/banner-first.avif 320w"
+                    sizes="(min-width: 650px) 50vw, 100vw"
+                />
 
                 <div className='absolute bottom-0 right-[240px]'>
-                    <img src="/assets/icon-arrow-long.svg" className='object-cover' />
+                    <img src="/assets/icon-arrow-long.svg" className='object-cover' alt="icon arrow down" />
                 </div>
             </div>
             <div className='flex justify-end'>
-                <img src="/assets/banner-last.png" className='object-cover h-[400px]' />
+                <img src="/assets/banner-last.avif" className='object-cover h-[400px]' alt="image banner"
+                    srcSet="/assets/banner-last.avif 640w,
+                 /assets/banner-last.avif 320w"
+                    sizes="(min-width: 650px) 50vw, 100vw"
+                />
             </div>
             <Link href="/pricing" className="absolute bottom-[40%] right-1/2 translate-x-1/2 text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:ring-primary-200 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:text-white  dark:focus:ring-primary-900">Try Now</Link>
         </div>
