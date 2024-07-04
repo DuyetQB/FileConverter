5281a22584b8821e6d6382b8d009fc10be915c1d
duyetqb <duyetqb@gmail.com>
2024-07-04 11:04:40 +0700

    update sitemap and pricing

--- src/app/components/payment/components/Card.tsx
+++ src/app/components/payment/components/Card.tsx
@@ -27,7 +27,7 @@ export default function Card(props: CardItemProps) {
                 ))}
 
             </ul>
-            <Link href="/convert-image" className="text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:ring-primary-200 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:text-white  dark:focus:ring-primary-900">Get started</Link>
+            <Link href={`${price === 0 ? '/convert-image':'sign-in'}`} className="text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:ring-primary-200 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:text-white  dark:focus:ring-primary-900">Get started</Link>
             </div>
         </>
     )
