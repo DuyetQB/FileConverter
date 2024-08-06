33c5743881adde4cae94490a08444d752e655d67
duyetqb <duyetqb@gmail.com>
2024-08-06 09:59:48 +0700

    rm checkout button

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -3,7 +3,6 @@ import React from 'react'
 import Banner from '../components/Banner'
 import Layout from '../components/layouts'
 import Gtag from '../components/Gtag';
-import ButtonCheckout from '../components/ButtonCheckout';
 // const BannerAds = dynamic(() => import('../components/BannerAds'), { ssr: false })
 
 const Page: React.FC = () => {
@@ -11,7 +10,6 @@ const Page: React.FC = () => {
     return (
         <Layout>
             <Banner />
-            <ButtonCheckout />
            {/* <BannerAds/> */}
            <Gtag />
         </Layout>
